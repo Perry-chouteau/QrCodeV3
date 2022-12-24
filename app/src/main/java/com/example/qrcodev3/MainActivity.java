b070b763e3b69bd5a0d3daf578e91895a767025e
Perry Chouteau <perry.chouteau@epitech.eu>
2022-12-24 21:37:43 +0100

    feat(QrCodeScanner): :sparkles: previewView link to Scanner<log.i(qrcode.data)>

--- app/src/main/java/com/example/qrcodev3/MainActivity.java
+++ app/src/main/java/com/example/qrcodev3/MainActivity.java
@@ -4,23 +4,44 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.Camera;
 import androidx.camera.core.CameraControl;
+import androidx.camera.core.CameraProvider;
 import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ExperimentalGetImage;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.CameraController;
 import androidx.camera.view.PreviewView;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
 
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Surface;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.Task;
 import com.google.common.util.concurrent.ListenableFuture;
+import com.google.mlkit.vision.barcode.BarcodeScanner;
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
+import com.google.mlkit.vision.barcode.BarcodeScanning;
+import com.google.mlkit.vision.barcode.common.Barcode;
+import com.google.mlkit.vision.common.InputImage;
 
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
     private PreviewView previewView;
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    private ImageCapture imageCapture;
+    private BarcodeScanner barcodeScanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +63,51 @@ public class MainActivity extends AppCompatActivity {
                 // This should never be reached.
             }
         }, ContextCompat.getMainExecutor(this));
+
+        BarcodeScannerOptions barcodeScannerOptions =
+                new BarcodeScannerOptions.Builder()
+                        .setBarcodeFormats(
+                                Barcode.FORMAT_QR_CODE,
+                                Barcode.FORMAT_AZTEC)
+                        .build();
+
+        barcodeScanner = BarcodeScanning.getClient(barcodeScannerOptions);
+    }
+
+    void launchThread() {
+        new Thread(() -> {
+            imageCapture.takePicture(
+                ContextCompat.getMainExecutor(this),
+                new ImageCapture.OnImageCapturedCallback() {
+                    @Override
+                    @ExperimentalGetImage
+                    public void onCaptureSuccess(@NonNull ImageProxy imageProxy) {
+                        Log.w("SUCCESS", "Capture");
+
+                        Image image = imageProxy.getImage();
+                        if (image == null) { return; }
+
+                        InputImage inputImage = InputImage.fromMediaImage(image, imageProxy.getImageInfo().getRotationDegrees());
+                        barcodeScanner.process(inputImage)
+                                .addOnSuccessListener(barcodes -> {
+                                    barcodes.forEach(barcode -> {
+                                        Log.w("SUCCESS", barcode.getDisplayValue());
+                                    });
+                                }).addOnFailureListener(error -> Log.e("Error", error.getMessage()));
+
+                        imageProxy.close();
+
+                        launchThread();
+                    }
+
+                    @Override
+                    public void onError(@NonNull ImageCaptureException exception) {
+                        launchThread();
+                        Log.e("ERROR", "Capture");
+                    }
+                }
+            );
+        }).start();
     }
 
     void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
@@ -54,8 +120,14 @@ public class MainActivity extends AppCompatActivity {
 
         preview.setSurfaceProvider(previewView.getSurfaceProvider());
 
-        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview);
+        imageCapture =
+            new ImageCapture.Builder()
+                    .setTargetRotation(Surface.ROTATION_0)
+                    .build();
+        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, imageCapture,  preview);
         CameraControl cameraControl = camera.getCameraControl();
         cameraControl.setLinearZoom((float)0.3);
+
+        launchThread();
     }
 }
